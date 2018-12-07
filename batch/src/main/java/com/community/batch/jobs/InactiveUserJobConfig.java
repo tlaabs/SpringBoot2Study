f515b87d4c055bc9e7caf273cd0bd6d19ea7e0fb
tlaabs <tlaabs@naver.com>
2018-12-07 17:50:06 +0900

    JpaPagingItemReader index 문제 해결

--- batch/src/main/java/com/community/batch/jobs/InactiveUserJobConfig.java
+++ batch/src/main/java/com/community/batch/jobs/InactiveUserJobConfig.java
@@ -12,15 +12,23 @@ import org.springframework.batch.core.configuration.annotation.StepBuilderFactor
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JpaPagingItemReader;
+import org.springframework.batch.item.support.ListItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.persistence.EntityManagerFactory;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @Configuration
 public class InactiveUserJobConfig {
+
+    private final static int CHUNK_SIZE = 15;
+    private final EntityManagerFactory entityManagerFactory;
 
     private UserRepository userRepository;
 
@@ -36,18 +44,43 @@ public class InactiveUserJobConfig {
     public Step inactiveJobStep(StepBuilderFactory stepBuilderFactory){
         return stepBuilderFactory.get("inactiveUserStep")
                 .<User, User> chunk(10) //커밋단위
-                .reader(inactiveUserReader())
+                .reader(inactiveUserJpaReader())
                 .processor(inactiveUserProcessor())
                 .writer(inactiveUserWriter())
                 .build();
     }
 
+    @Bean(destroyMethod = "")
+    @StepScope
+    public JpaPagingItemReader<User> inactiveUserJpaReader(){
+        JpaPagingItemReader<User> jpaPagingItemReader =
+                //https://jojoldu.tistory.com/337 페이징 문제
+                new JpaPagingItemReader(){
+                    @Override
+                    public int getPage() {
+                        return 0;
+                    }
+                };
+        jpaPagingItemReader.setQueryString("select u from User as u where u.updatedDate < :updatedDate and u.status = :status");
+
+        Map<String, Object> map = new HashMap<>();
+        LocalDateTime now = LocalDateTime.now();
+        map.put("updatedDate", now.minusYears(1));
+        map.put("status", UserStatus.ACTIVE);
+
+        jpaPagingItemReader.setParameterValues(map);
+
+        jpaPagingItemReader.setEntityManagerFactory(entityManagerFactory);
+        jpaPagingItemReader.setPageSize(CHUNK_SIZE);
+        return jpaPagingItemReader;
+    }
+
     @Bean
     @StepScope //각 Step의 실행마다 새로 빈을 만들기 때문에 지연 생성이 가능하다?
-    public QueueItemReader<User> inactiveUserReader(){
+    public ListItemReader<User> inactiveUserReader(){
         List<User> oldUsers =
                 userRepository.findByUpdatedDateBeforeAndStatusEquals(LocalDateTime.now().minusYears(1), UserStatus.ACTIVE);
-        return new QueueItemReader<>(oldUsers);
+        return new ListItemReader<>(oldUsers);
     }
 
     public ItemProcessor<User, User> inactiveUserProcessor(){
